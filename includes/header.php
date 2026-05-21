44da245e4a962c7abbb6c87c4f6c00600b84f6ec
Shrinivas Biradar <biradarshri810@gmail.com>
2026-05-21 15:37:07 +0530

    Add favicon and theme color links to header

--- includes/header.php
+++ includes/header.php
@@ -19,6 +19,13 @@ if (!isset($base)) {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;600;700;800&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="<?php echo $base; ?>assets/img/favicon.ico">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $base; ?>assets/img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $base; ?>assets/img/favicon-16x16.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $base; ?>assets/img/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="<?php echo $base; ?>assets/img/android-chrome-192x192.png">
+  <link rel="icon" type="image/png" sizes="512x512" href="<?php echo $base; ?>assets/img/android-chrome-512x512.png">
+  <meta name="theme-color" content="#ffffff">
   <link rel="stylesheet" href="<?php echo $base; ?>assets/css/style.css">
 </head>
 <body>
